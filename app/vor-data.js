2a964d470fd8b99a596fb06efab612032080a72c
Anna Nikiel <anna@annanikiel.com>
2026-01-01 18:11:27 +0000

    Add VOR_DATA array with location information

---
+++ app/vor-data.js
@@ -0,0 +1,5 @@
+window.VOR_DATA = [
+  { id: "WAL", name: "WAL (Wallasey)", lat: 53.391899, lon: -3.134440 },
+  { id: "POL", name: "POL (Pole Hill)", lat: 53.743900, lon: -2.103330 },
+  { id: "MCT", name: "MCT (Example)",  lat: 53.000000, lon: -2.500000 },
+];
